073620b3dbb49a722df81ebee55a4c387776ae1a
realjosecorreia <correia@iterasys.com.br>
2020-02-01 11:50:03 -0300

    Carga do projeto completo
    Código criado na aula da semana passada, em que revisamos um script que faz uma busca no Google

---
+++ RevisaGoogle/src/test/java/revisagoogle/Runner.java
@@ -0,0 +1,23 @@
+package revisagoogle;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		dryRun = false,
+		monochrome = true,
+		features = { "src/test/resources/" },
+		glue = { "revisagoogle/" },
+		plugin = { "pretty",
+				   "html:target/relatorio1.html",
+				   "json:target/relatorio1.json",
+				   "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/relatorio2.html"
+		}
+)
+
+public class Runner {
+
+}
